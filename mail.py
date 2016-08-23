4ad6f6179ae228126d26121f9151079b7b1be856
mat3q <github@mat3.pro>
2016-08-23 17:33:03 +0300

    .Xdefault & mail.py

---
+++ mail.py
@@ -0,0 +1,12 @@
+﻿# author mat3
+# hello python world
+from xml.dom import minidom
+import urllib.request as urllib2
+
+url = urllib2.HTTPPasswordMgrWithDefaultRealm()
+url.add_password(None, "htts://mail.google.com/mail/feed/atom", "login(without @gmail)", "pass")
+urllib2.install_opener(urllib2.build_opener(urllib2.HTTPBasicAuthHandler(url)))
+
+dom = minidom.parse(urllib2.urlopen("https://mail.google.com/mail/feed/atom"))
+count = dom.getElementsByTagName('fullcount')[0].childNodes[0].nodeValue
+print(count)
